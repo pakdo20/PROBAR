fdd68d7e191ae00b98574ffb1f7b03a5a009c69b
pakdo20 <u20181b515@upc.edu.pe>
2022-01-12 01:21:09 -0500

    added yoniii

--- src/main/java/pe/edu/upc/bodega/SpringBodegappApplication.java
+++ src/main/java/pe/edu/upc/bodega/SpringBodegappApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.bodega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pe.edu.upc.bodega"})
 public class SpringBodegappApplication {
 
 	public static void main(String[] args) {
